c33a42dad6b3cce8ecc19dcab97fea8ac422a6f7
Gavin Staniforth <gsdevme@users.noreply.github.com>
2023-04-04 23:19:13 +0100

    feat: add basic init of display

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "display.cpp"
 
 int main() {
     stdio_init_all();
+
+    display_gpio_init();
+    display_spi_init();
+    display_init();
 
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
@@ -11,6 +16,6 @@ int main() {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(250);
 
-        printf("Hello, world!");
+        printf("Hello, world!\r\n");
     }
 }
